4c0a0e67023748611518f2efeb269f055e2ed7ef
Özge Güney <43542812+ozgeguney@users.noreply.github.com>
2023-04-25 13:41:01 +0300

    Add files via upload

---
+++ Preprocessing.py
@@ -0,0 +1,150 @@
+import nltk
+from nltk.corpus import twitter_samples
+import matplotlib.pyplot as plt
+import random
+
+#nltk.download('twitter_samples')
+all_positive_samples = twitter_samples.strings("positive_tweets.json")
+all_negative_samples = twitter_samples.strings("negative_tweets.json")
+print(type(all_positive_samples[0]))
+
+fig = plt.figure(figsize=(8, 6))
+labels = 'Positives', 'Negative'
+sizes = [len(all_positive_samples), len(all_negative_samples)]
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+plt.axis('equal')
+#plt.show()
+
+# print('\033[92m' + all_positive_samples[random.randint(0,5000)])
+# print('\033[91m' + all_negative_samples[random.randint(0,5000)])
+
+tweet = all_positive_samples[2277]
+print(tweet)
+
+import re                                  # library for regular expression operations
+import string                              # for string operations
+
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
+
+print('\033[92m' + tweet)
+print('\033[94m')
+
+# remove old style retweet text "RT"
+tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+# remove hyperlinks
+tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+
+# remove hashtags
+# only removing the hash # sign from the word
+tweet2 = re.sub(r'#', '', tweet2)
+
+print(tweet2)
+
+print()
+print('\033[92m' + tweet2)
+print('\033[94m')
+
+# instantiate tokenizer class
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+
+# tokenize tweets
+tweet_tokens = tokenizer.tokenize(tweet2)
+
+print()
+print('Tokenized string:')
+print(tweet_tokens)
+
+#Import the english stop words list from NLTK
+stopwords_english = stopwords.words('english')
+
+print('Stop words\n')
+print(stopwords_english)
+
+print('\nPunctuation\n')
+print(string.punctuation)
+
+print()
+print('\033[92m')
+print(tweet_tokens)
+print('\033[94m')
+
+tweets_clean = []
+
+for word in tweet_tokens: # Go through every word in your tokens list
+    if (word not in stopwords_english and  # remove stopwords
+        word not in string.punctuation):  # remove punctuation
+        tweets_clean.append(word)
+
+print('removed stop words and punctuation:')
+print(tweets_clean)
+
+print()
+print('\033[92m')
+print(tweets_clean)
+print('\033[94m')
+
+# Instantiate stemming class
+stemmer = PorterStemmer()
+
+# Create an empty list to store the stems
+tweets_stem = []
+
+for word in tweets_clean:
+    stem_word = stemmer.stem(word)  # stemming word
+    tweets_stem.append(stem_word)  # append to the list
+
+print('stemmed words:')
+print(tweets_stem)
+
+def process_tweet(tweet):
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and  # remove stopwords
+                word not in string.punctuation):  # remove punctuation
+            # tweets_clean.append(word)
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+# choose the same tweet
+tweet = all_positive_samples[2277]
+
+print()
+print('\033[92m')
+print(tweet)
+print('\033[94m')
+
+# call the imported function
+tweets_stem = process_tweet(tweet); # Preprocess a given tweet
+
+print('preprocessed tweet:')
+print(tweets_stem) # Print the result
