d57cba89c0f352822ae68bac0affd73c252f66f9
Özge Güney <43542812+ozgeguney@users.noreply.github.com>
2023-05-06 02:29:56 +0300

    Add files via upload

--- NaiveBayes_NLP.py
+++ NaiveBayes_NLP.py
@@ -1,19 +1,10 @@
-
-import pdb
 from nltk.corpus import stopwords, twitter_samples
 import numpy as np
-import pandas as pd
-import nltk
+import re
 import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
-from os import getcwd
-#import w2_unittest
-import re                                  # library for regular expression operations
-import string                              # for string operations
-
-from nltk.corpus import stopwords          # module for stop words that come with NLTK
-from nltk.stem import PorterStemmer        # module for stemming
-from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
 
 # get the sets of positive and negative tweets
 all_positive_tweets = twitter_samples.strings('positive_tweets.json')
@@ -62,7 +53,7 @@ def process_tweet(tweet):
     return tweets_clean
 
 # print cleaned tweet
-print(process_tweet(custom_tweet))
+#print(process_tweet(custom_tweet))
 
 
 def count_tweets(result, tweets, ys):
@@ -77,10 +68,6 @@ def count_tweets(result, tweets, ys):
                 result[pair] = 1
     return result
 
-result = {}
-tweets = ['i am happy', 'i am tricked', 'i am sad', 'i am tired', 'i am tired']
-ys = [1, 0, 0, 0, 0]
-print(count_tweets(result, tweets, ys))
 freqs = count_tweets({}, train_x, train_y)
 
 def lookup(freqs, word, label):
@@ -90,16 +77,93 @@ def lookup(freqs, word, label):
         n = freqs[pair]
     return n
 
+def train_naive_bayes(freqs, train_x, train_y):
 
-vocab = len(freqs)
-V = {}
-unique = set(freqs)
-#print(list(freqs.keys()))
-vocabL = []
-for pair in list(freqs.keys()):
-    vocabL.append(pair[0])
-vocab = set(vocabL)
-print(len(vocab))
+    loglikelihood = {}
+    logprior = 0
+    vocabL = []
+    for pair in list(freqs.keys()):
+        vocabL.append(pair[0])
+    vocab = set(vocabL)
+
+    V = len(vocab)
+    N_pos = N_neg = 0
+    for pair in freqs.keys():
+        if pair[1] > 0:
+            N_pos += freqs[pair]
+        else:
+            N_neg += freqs[pair]
+
+    D = len(train_x)
+    D_pos = len(train_y[train_y == 1])
+    D_neg = len(train_y[train_y == 0])
+    logprior = np.log(D_pos) - np.log(D_neg)
+
+    for word in vocab:
+        freq_pos = lookup(freqs, word, 1)
+        freq_neg = lookup(freqs, word, 0)
+        p_w_pos = (freq_pos + 1) / (N_pos + V)
+        p_w_neg = (freq_neg + 1) / (N_neg + V)
+        loglikelihood[word] = np.log(p_w_pos) - np.log(p_w_neg)
+
+    return logprior, loglikelihood
+
+def naive_bayes_predict(tweet, logprior, loglikelihood):
+
+    word_l = process_tweet(tweet)
+    p = 0
+    p += logprior
+
+    for word in word_l:
+        if word in loglikelihood:
+            p += loglikelihood[word]
+    return p
+
+logprior,loglikelihood = train_naive_bayes(freqs, train_x, train_y)
+
+my_tweet = 'She smiled.'
+p = naive_bayes_predict(my_tweet, logprior, loglikelihood)
+
+def test_naive_bayes(test_x, test_y, logprior, loglikelihood, naive_bayes_predict=naive_bayes_predict):
+
+    y_hats = []
+    for tweet in test_x:
+        if naive_bayes_predict(tweet, logprior, loglikelihood) > 0:
+            y_hat_i = 1
+        else:
+            y_hat_i = 0
+        y_hats.append(y_hat_i)
+    error = (np.sum(np.abs(y_hats - test_y)))/(len(test_y))
+    accuracy = 1 - error
+
+    return accuracy
+
+print("Naive Bayes accuracy = %0.4f" %
+      (test_naive_bayes(test_x, test_y, logprior, loglikelihood)))
+
+for tweet in ['I am happy', 'I am bad', 'this movie should have been great.', 'great', 'great great', 'great great great', 'great great great great']:
+    p = naive_bayes_predict(tweet, logprior, loglikelihood)
+    print(f'{tweet} -> {p:.2f}')
+
+my_tweet = 'you are bad :('
+print(naive_bayes_predict(my_tweet, logprior, loglikelihood))
+
+def get_ratio(freqs, word):
+
+    pos_neg_ratio = {'positive': 0, 'negative': 0, 'ratio': 0.0}
+    pos_neg_ratio['positive'] = lookup(freqs, word, 1)
+    pos_neg_ratio['negative'] = lookup(freqs, word, 0)
+    pos_neg_ratio['ratio'] = (pos_neg_ratio['positive'] + 1) / (pos_neg_ratio['negative'] + 1)
+    
+    return pos_neg_ratio
+
+print(get_ratio(freqs, 'happi'))
+
+my_tweet = 'I am happy because I am learning :)'
+
+p = naive_bayes_predict(my_tweet, logprior, loglikelihood)
+print(p)
+
 
 
 
